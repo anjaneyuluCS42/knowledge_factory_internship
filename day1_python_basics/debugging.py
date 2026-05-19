5de77660c11f103a9c4dfcd8f4fb4f3d64ec2b24
anjaneyuluCS42 <anjaneyuluCS42-G6@coastalseven.com>
2026-05-19 18:22:02 +0530

    Day 1 basics and debugging completed

--- day1_python_basics/debugging.py
+++ day1_python_basics/debugging.py
@@ -1,15 +1,9 @@
-# def add_numbers(a, b):
-#     # మనం ఇక్కడ వాల్యూస్ సరిగ్గా ఉన్నాయో లేదో చూద్దాం
-#     print(f"DEBUG: a విలువ {a}, b విలువ {b}") 
-#     return a + b
+def add_numbers(a, b):
+    
+    print(f"DEBUG: a  {a}, b  {b}") 
+    return a + b
 
-# result = add_numbers(10, "20") #20 a string
-# print(result)
-# print(print("hello"))
+result = add_numbers(10, "20") 
+print(result)
+print(print("hello"))
 
-def number(a,b):
-    print(a+b)
-    return(a+b)
-
-x = number(2,3)
-print(x)
